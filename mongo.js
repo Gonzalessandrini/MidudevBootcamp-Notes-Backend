f0e0a85a9bd62b59ad76b5cfe5a7d0bd28dfbd9e
Gonzalo <alessandrinigonzalo3@gmail.com>
2023-08-13 22:50:47 -0300

    prueba 10

--- mongo.js
+++ mongo.js
@@ -4,7 +4,7 @@ const {MONGO_DB_URI, MONGO_DB_TEST, NODE_ENV}= process.env
 
 const conectionString= NODE_ENV === 'test'
 ? MONGO_DB_TEST
-: MONGO_DB_URI
+: 'mongodb+srv://alessandrinigonzalo3:44662130Xds12@cluster0.qprecjz.mongodb.net/app-notes?retryWrites=true&w=majority'
 
 
 mongoose.connect(conectionString)
